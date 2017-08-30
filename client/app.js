29c454f10a0cffa1451ac7a99d874966c1c47fb1
Meagan Blais <meaganblais@gmail.com>
2017-08-30 11:00:21 -0400

    adding part 2 & part 3

--- client/app.js
+++ client/app.js
@@ -1,7 +1,18 @@
 // window.alert("JS file is connected")
 
-$('form').on('submit',function () {
-  var text = $('#message').val();
-  alert(text);
-  return false;
-})
+  let socket = io();
+
+  $('form').on('submit',function () {
+    let message = $('#message').val();
+    let initials = $('#initials').val();
+
+    let fullmsg = {'name': initials, 'text': message}
+    socket.emit('message', fullmsg);
+    $('#message').val('');
+    $('#initials').val('');
+    return false;
+  })
+
+  socket.on('message', function (msg) {
+    $('<li>').text(`${msg.name} says: ${msg.text}`).appendTo('#history');
+  });
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('hello from the node script!');
+// console.log('hello from the node script!'); //test in terminal by running 'node server.js'
+
+let express = require('express');
+let app = express();
+
+let http = require('http');
+let server = http.Server(app);
+
+app.use(express.static('client'));
+
+let io = require('socket.io') (server);
+
+io.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    io.emit('message', msg)
+  })
+})
+
+server.listen(8080, function () {
+  console.log('Chat server running');
+});
